8208e22e600f57388d1849279e7b0b341e3a011f
elizamkh <elizamkh@mail.ru>
2023-10-29 18:37:29 +0500

    ок

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+
 let username = prompt ("Введите имя:")
 
-let myTitle = document.getElementById("title")
+let title = document.getElementById("title")
 
 if (username) {
-    myTitle.innerHTML = "Привет, " + username
+    title.innerHTML = "Ваш список дел, " + username
 } else {
-    myTitle.innerHTML = "Привет, анонимный пользователь)"
+    title.innerHTML = "Ваш список дел, анонимный пользователь)"
 }
 
 let count = 0
 
-function add() {
-    let myConteiner = document.getElementById("conteiner")
-    let myTitle = document.getElementById("title")
-    let myInput = document.getElementById("input")
-
-    let value = myInput.value
-
-    console.log(myInput)
-    console.log(value)
-
-    myTitle.innerHTML = "Ваш список дел,"
-
+function addCase() {
     count = count + 1
-
-    myConteiner.innerHTML = myConteiner.innerHTML + "<p> " + value + "</p>"
-}
+    let error = document.getElementById('error')
+    if (count > 5) {
+        error.innerHTML = "Можно ввести до 5 дел"
+        return    }
+        
+    let cases = document.getElementById("cases")
+    let title = document.getElementById("title")
+    let input = document.getElementById("input")
+    let value = input.value
  
+    cases.innerHTML = cases.innerHTML + "<li> " + value + "</li>"}
+   
+
+
